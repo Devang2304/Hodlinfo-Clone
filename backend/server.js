86ffc8b123a99e7c0c076409eb51b6e922678336
Devang2304 <devangvartak2304@gmail.com>
2023-06-08 04:15:17 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 }).catch(err=>console.log(err.message));
 
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.resolve(__dirname + "../frontend/build")));
-    app.get("*",(req,res)=>res.sendFile(path.resolve(__dirname + "../frontend/build/index.html")));
-}
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.resolve(__dirname + "../frontend/build")));
+//     app.get("*",(req,res)=>res.sendFile(path.resolve(__dirname + "../frontend/build/index.html")));
+// }
 
 const PORT =process.env.PORT || 5000;
 app.listen(PORT,()=>{
