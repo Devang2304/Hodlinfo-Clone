43be5cc005664803f98201645483916b14a837bd
Devang2304 <devangvartak2304@gmail.com>
2023-06-08 04:32:41 +0530

    server link added to api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const URL = 'https://hodlinfo-clone-3hut.onrender.com/';
+const URL = 'https://hodlinfo-clone-devang.onrender.com';
 
 export const getData = async () =>{
     try {
         return await axios.get(`${URL}/`);
     } catch (error) {
-        console.log("Error while calling getData api",error);
+        console.log("Error while calling getData api",error)
     }
 }
